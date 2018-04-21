45dd3c62ec377542a1572cdf1c7652d917fd4112
Pavel Karateev <karateev.pavel@ya.ru>
2018-04-21 22:01:34 +0300

    Add game sh

---
+++ guessinggame.sh
@@ -0,0 +1,18 @@
+function game {
+    echo "Game starts"
+    local real=$(ls -l . | egrep -c '^-')
+    local guess
+
+    until [[ $real -eq $guess ]]; do
+        echo -n "Guess the number of files in cwd: " && read guess
+        if [[ $real -lt $guess ]]; then
+            echo "Your guess is too big"
+        elif [[ $real -gt $guess ]]; then
+            echo "Your guess it too low"
+        fi
+    done
+
+    echo "You are correct"
+}
+
+game
